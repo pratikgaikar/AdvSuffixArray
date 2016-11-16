7fbfca52c4f72e1e67589a3489d0c83956951344
root <pratik.gaikar2008@gmail.com>
2016-11-15 19:20:01 -0800

    added code to prase file

--- main.cc
+++ main.cc
@@ -2,6 +2,7 @@
 #include <map>
 #include <vector>
 #include <cstring>
+#include <fstream>
 using namespace std;
 
 
@@ -16,35 +17,35 @@ vector<int> build_suffix_array(string text)
        	map<string,int>:: iterator it = hash_map.begin();
     	for (it=hash_map.begin(); it!=hash_map.end(); ++it)
     	{
-        	cout << it->first << " => " << it->second << '\n';
+        	//cout << it->first << " => " << it->second << '\n';
         	suffix_array.push_back(it->second);
     	}
 
 	return suffix_array;
 }
 
-vector<int> find_pattern(vector<int> sa, string search_str,string str)
+vector<int> find_pattern(vector<int> suffix_array, string search_str,string str)
 {
 	vector<int> pattern_index;
 	int len = strlen(search_str.c_str());
-	int low = 0 , high = sa.size() - 1;
+	int low = 0 , high = suffix_array.size() - 1;
     	while(low <= high)
     	{
         	int mid = low + (high - low)/2;
-        	string mid_str = str.substr(sa.at(mid));
+        	string mid_str = str.substr(suffix_array.at(mid));
         	string i_str;
         	if(strncmp(search_str.c_str(),mid_str.c_str(),len) == 0)
         	{ 
             		int i = mid-1;
-            		cout<<"\nFound at "<<mid_str<<"Index -->"<<sa.at(mid);
-			pattern_index.push_back(sa.at(mid));
+            		cout<<"\nFound at"<<mid_str;
+			pattern_index.push_back(suffix_array.at(mid));
             		while(i >= low)
             		{
-                		i_str = str.substr(sa.at(i));
+                		i_str = str.substr(suffix_array.at(i));
                 		if(strncmp(search_str.c_str(),i_str.c_str(),len) == 0)
 				{
-					pattern_index.push_back(sa.at(i));
-                    			cout<<"\nFound at "<<i_str<<"Index -->"<<sa.at(i);
+					pattern_index.push_back(suffix_array.at(i));
+                    			cout<<"\nFound at "<<i_str;
 				}                		
 				else
                     			break;
@@ -54,11 +55,11 @@ vector<int> find_pattern(vector<int> sa, string search_str,string str)
             		i = mid +1;
             		while(i <= high)
             		{
-                		i_str = str.substr(sa.at(i));
+                		i_str = str.substr(suffix_array.at(i));
                 		if(strncmp(search_str.c_str(),i_str.c_str(),len) == 0)
 				{
-					pattern_index.push_back(sa.at(i));
-                    			cout<<"\nFound at "<<i_str<<"Index -->"<<sa.at(i);
+					pattern_index.push_back(suffix_array.at(i));
+                    			cout<<"\nFound at "<<i_str;
 				}
                 		else
                     			break;
@@ -78,14 +79,34 @@ vector<int> find_pattern(vector<int> sa, string search_str,string str)
     	}       
 	return pattern_index;
 }
+
 int main()
 {
-	string str = "aba$babbb$";
+	string line;
+ 	string str;
+  	string a = ">";
+  	ifstream myfile ("/home/megatron/Human_Genome.transcripts.fa");
+	if (myfile.is_open())
+	{
+		while ( getline (myfile,line) )
+		{
+        		if (strncmp (line.c_str(),a.c_str(),1) != 0)
+    			{
+      	 			str.append(line);
+      				str.append("$"); 
+    			}
+    		}
+    		cout << str.length() <<'\n';
+    		myfile.close();
+  	}
+	else cout << "Unable to open file"; 
+	
+	//string str = "aba$babbb$";
 	vector<int> suffix_array; 
 	suffix_array = build_suffix_array(str); 
-	string search_str = "ba"; 
-	find_pattern(suffix_array,search_str,str);	
+	
+	//string search_str = "ba"; 
+	//find_pattern(suffix_array,search_str,str);	
     	return 0;
 }
-
 
